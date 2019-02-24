9062624c194306096ef42ea89481cb62dbc844db
jocular-markup-2001 <andreacannuni@gmail.com>
2019-02-24 20:07:15 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -3,7 +3,8 @@ var recipes = {
 }
 
 function updateObjectWithKeyAndValue(object, key, value) {
-  return Object.assign(object, {[key]: value});
+  Object.assign(object, {[key]: value});
+  return object;
 }
 
 function destructivelyUpdateObjectWithKeyAndValue(object, key, value) {
